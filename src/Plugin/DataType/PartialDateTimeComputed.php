f9ade2b1943940285dc3aed41aa4f3192d88aed1
Tobias Stöckler <tst@bio.logis.de>
2017-01-24 03:57:47 +0100

    Fix re-calculated of PartialDateTimeComputed is not triggered

--- src/Plugin/DataType/PartialDateTimeComputed.php
+++ src/Plugin/DataType/PartialDateTimeComputed.php
@@ -12,46 +12,63 @@ class PartialDateTimeComputed extends TypedData {
   /**
    * An array of date information for the partial date.
    *
-   * @var array
+   * @var array|null
    */
-  protected $partialDate = [];
+  protected $partialDate = NULL;
 
   /**
    * {@inheritdoc}
    */
   public function getValue() {
-    if (!$this->partialDate) {
-      /** @var \Drupal\Core\Field\FieldItemInterface $item */
-      $item = $this->getParent();
-      $estimates = $item->getFieldDefinition()->getSetting('estimates');
+    if ($this->partialDate !== NULL) {
+      return $this->partialDate;
+    }
 
-      $data = $item->data;
-      foreach (array_keys(partial_date_components()) as $property) {
-        $this->partialDate[$property] = '';
-        if (empty($data[$property . '_estimate_from_used'])) {
-          $this->partialDate[$property] = $item->{$property};
-        }
+    /** @var \Drupal\Core\Field\FieldItemInterface $item */
+    $item = $this->getParent();
+    $estimates = $item->getFieldDefinition()->getSetting('estimates');
+    $range_setting = $this->definition->getSetting('range');
 
-        $this->partialDate[$property . '_estimate'] = '';
-        if ($property !== 'timezone' && !empty($data[$property . '_estimate'])) {
-          $value = $data[$property . '_estimate'];
+    $data = $item->data;
+    foreach (array_keys(partial_date_components()) as $component) {
+      $property = $component;
+      if ($range_setting === 'to') {
+        $property .= '_to';
+      }
+      $this->partialDate[$component] = '';
+      if (empty($data[$component . '_estimate_from_used'])) {
+        $this->partialDate[$component] = $item->{$property};
+      }
 
-          $this->partialDate[$property . '_estimate'] = $value;
-          $this->partialDate[$property . '_estimate_label'] = '';
-          $this->partialDate[$property . '_estimate_value'] = NULL;
+      $this->partialDate[$component . '_estimate'] = '';
+      if ($component !== 'timezone' && !empty($data[$component . '_estimate'])) {
+        $value = $data[$component . '_estimate'];
 
-          if ($value) {
-            if (!empty($estimates[$property][$value])) {
-              $this->partialDate[$property . '_estimate_label'] = $estimates[$property][$value];
-            }
-            list($from, $to) = explode('|', $value);
-            $range_setting = $this->definition->getSetting('range');
-            $this->partialDate[$property . '_estimate_value'] = ($range_setting === 'from') ? $from : $to;
+        $this->partialDate[$component . '_estimate'] = $value;
+        $this->partialDate[$component . '_estimate_label'] = '';
+        $this->partialDate[$component . '_estimate_value'] = NULL;
+
+        if ($value) {
+          if (!empty($estimates[$component][$value])) {
+            $this->partialDate[$component . '_estimate_label'] = $estimates[$component][$value];
           }
+          list($from, $to) = explode('|', $value);
+          $this->partialDate[$component . '_estimate_value'] = ($range_setting === 'from') ? $from : $to;
         }
       }
     }
     return $this->partialDate;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setValue($value, $notify = TRUE) {
+    $this->partialDate = $value;
+    // Notify the parent of any changes.
+    if ($notify && isset($this->parent)) {
+      $this->parent->onChange($this->name);
+    }
   }
 
 }
